4df267babb6d6058bc7e1d2edafa2921b5ab9e42
narek <narek.okroyan@outlook.com>
2024-06-25 15:27:45 +0200

    fix training

--- src/conf.py
+++ src/conf.py
@@ -161,7 +161,7 @@ class Conf_COAD_DUMMY_LABEL(Conf_COAD):
         self.NAME = 'hsa_mir_1269a_lo_vs_hi'
         self.DUMMY = True
         self.CLINICAL_LABEL_COLS = ['Dummy']
-        self.LOAD_WEIGHTS_PATH = '/home/narek/Desktop/FUB/SOMMER/DSL/PathoMCH/out/hsa_mir_1269a_lo_vs_hi/auc'
+        self.LOAD_WEIGHTS_PATH = '../out/hsa_mir_1269a_lo_vs_hi_zoom_20_round_0_2024_06_22_23_20_13/auc'
         self.GCS_PATTERN = 'gs://dsitls-project/data/{}{{}}*.tfrec'.format(self.NAME)
         self.GCS_PATTERN_LOCAL = '../res/{}/{{}}*.tfrec'.format(self.NAME)
         self.GCS_PATTERN_PER_SAMPLE = '../res/all_samples_dummy_labels/*tfrecords'
