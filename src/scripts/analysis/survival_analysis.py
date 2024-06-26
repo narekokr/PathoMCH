a8da7d5cca3857d910b44a35489a35bb75d0bf07
Mahias <mahiasw224@zedta.fu-berlin.de>
2024-06-26 17:50:47 +0200

    added possibility to split survival analysis by more than one HT Index threshold

--- src/scripts/analysis/survival_analysis.py
+++ src/scripts/analysis/survival_analysis.py
@@ -8,20 +8,18 @@ from difflib import get_close_matches
 import os
 
 
-def prepare_data(file_path, hi_list=None, hti_threshold=0.5):
+def prepare_data(file_path, time_col, event_col, hi_list=None, hti_threshold=0.5, split_mode='binary'):
     # Load the data
     data = pd.read_csv(file_path)
-
-    # Define the columns for survival analysis
-    time_col = 'Overall Survival (Months)'
-    event_col = 'Overall Survival Status'
-
     # Convert the event column to binary (0 for living/censored, 1 for deceased/event occurred)
     data[event_col] = data[event_col].map(lambda x: 0 if x == '0:LIVING' else 1)
+
+    # Identify heterogeneity index column
     potential_colnames_HTI = ['HI', 'HTI', 'HT-Index', 'HT - Index', 'heterogeneity index']
     hetero_index_colname = next((col for col in data.columns
-                                 if any(get_close_matches(col.lower(), [s.lower()],cutoff=0.7)
-                                    for s in potential_colnames_HTI)), None)
+                                 if any(get_close_matches(col.lower(), [s.lower()], cutoff=0.7)
+                                        for s in potential_colnames_HTI)), None)
+
     if hetero_index_colname:
         hi_col = data[hetero_index_colname]
     elif hi_list is not None:
@@ -30,7 +28,7 @@ def prepare_data(file_path, hi_list=None, hti_threshold=0.5):
         if len(hi_col) != len(data):
             raise ValueError("Length of HI list does not match the number of rows in the CSV")
     else:
-        # no HI can be found, generate randomly for test purposes
+        # Generate HI values randomly for test purposes
         # np.random.seed(42)  # For reproducibility
         hi_col = np.random.normal(loc=0.5, scale=0.15, size=len(data))
         hi_col = np.clip(hi_col, 0, 1)
@@ -38,9 +36,22 @@ def prepare_data(file_path, hi_list=None, hti_threshold=0.5):
     # Add the HI column to the data
     data['HI'] = hi_col
 
-    # Group data based on HI
-    data['HI Group'] = data['HI'].apply(lambda x: f"HT-Index <= {hti_threshold}"
-                                if x <= hti_threshold else f"HT-Index > {hti_threshold}")
+    # Determine the group based on HI thresholds
+    if isinstance(hti_threshold, list) and len(hti_threshold) == 2:
+        lower, upper = sorted(hti_threshold)
+        if split_mode == 'ternary':
+            data['HI Group'] = data['HI'].apply(lambda x: f'HT-Index ≤ {lower}' if x <= lower else
+            (f'{lower} < HT-Index ≤ {upper}' if x <= upper else
+             f'HT-Index > {upper}'))
+        else:
+            data['HI Group'] = data['HI'].apply(lambda x: f'HT-Index ≤ {lower}' if x <= lower else
+            (f'HT-Index > {upper}' if x > upper else
+             'Excluded'))
+            data = data[data['HI Group'] != 'Excluded']
+    else:
+        hti_threshold = hti_threshold[0]
+        data['HI Group'] = data['HI'].apply(lambda x: f'HT-Index ≤ {hti_threshold}'
+        if x <= hti_threshold else f'HT-Index > {hti_threshold}')
 
     # Drop rows with missing values in the survival columns
     km_data = data[[time_col, event_col, 'HI Group']].dropna()
@@ -48,14 +59,21 @@ def prepare_data(file_path, hi_list=None, hti_threshold=0.5):
     return km_data
 
 
-def plot_kaplan_meier(data, time_col, event_col, group_col, p_value, group_sizes, hti_threshold=0.5):
+def plot_kaplan_meier(data, time_col, event_col, group_col, p_value, group_sizes, hti_threshold):
     kmf = KaplanMeierFitter()
 
     # Plot survival curves for each group
-    for group in data[group_col].unique():
+    for group in data[group_col].sort_values().unique():
         group_data = data[data[group_col] == group]
-        kmf.fit(group_data[time_col], event_observed=group_data[event_col], label=f'Group {group}')
-        kmf.plot_survival_function()
+        kmf.fit(group_data[time_col], event_observed=group_data[event_col], label=group)
+        # make sure color coding is consistent
+        if ">" in group and not "≤" in group:
+            color = '#1f77b4' # blue
+        elif "≤" in group and not ">" in group and not "<" in group:
+            color = '#ff7f0e' # orange
+        else:
+            color = 'green'
+        kmf.plot_survival_function(color=color)
 
     plt.title('Kaplan-Meier Survival Curve by Group')
     plt.xlabel('Time (Months)')
@@ -63,30 +81,51 @@ def plot_kaplan_meier(data, time_col, event_col, group_col, p_value, group_sizes
     plt.legend()
 
     # Add text box with additional information
-    textstr = '\n'.join((
-        f'n ≤ {hti_threshold}: {group_sizes[0]}',
-        f'n > {hti_threshold}: {group_sizes[1]}',
-        "",
-        f'p-value (Log-rank test): {p_value:.4f}'))
-    plt.gca().text(0.05, 0.05, textstr, transform=plt.gca().transAxes, fontsize=9,
-                   verticalalignment='bottom')
+    textstr = '\n'.join([
+                            f'{str(group).replace("HT-Index", "n")}: {size}'
+                            for group, size in zip(data[group_col].unique(), group_sizes)
+                        ] + [f"", f'p-val: {p_value:.2f}'])
+
+    plt.gca().text(0.05, 0.05, textstr, transform=plt.gca().transAxes, fontsize=9, verticalalignment='bottom')
 
     plt.show()
 
-def perform_logrank_test(data, time_col, event_col, group_col, hti_threshold=0.5):
-    group1 = data[data[group_col] == f"HT-Index <= {hti_threshold}"]
-    group2 = data[data[group_col] == f"HT-Index > {hti_threshold}"]
+
+def perform_logrank_test(data, time_col, event_col, group_col):
+    unique_groups = data[group_col].unique()
+    group_data = [data[data[group_col] == group] for group in unique_groups]
 
     # make sure data is correctly split by string match
-    assert(not group1.empty)
-    assert(not group2.empty)
+    for dat in group_data:
+        assert (not dat.empty)
 
-    results = logrank_test(group1[time_col], group2[time_col], event_observed_A=group1[event_col], event_observed_B=group2[event_col])
+    if len(unique_groups) == 2:
+        results = logrank_test(group_data[0][time_col], group_data[1][time_col],
+                               event_observed_A=group_data[0][event_col],
+                               event_observed_B=group_data[1][event_col])
+    elif len(unique_groups) == 3:
+        results = logrank_test(group_data[0][time_col], group_data[2][time_col],
+                               event_observed_A=group_data[0][event_col],
+                               event_observed_B=group_data[2][event_col])
+    else:
+        raise ValueError("The data should be split into either 2 or 3 groups based on the HTI thresholds.")
+
     return results
 
 
-def main(file_path, hi_file_path=None):
-    hti_threshold = 0.7
+def main(file_path, time_col, event_col, hi_file_path=None, hti_threshold=0.5, split_mode='binary'):
+    """
+    :param file_path: clinical.csv, clinical data file holding survival information and optionally a column holding
+    the heterogeneity value with a column name from 'potential_colnames_HTI'
+    :param time_col: column name in clinical.csv giving survival time information
+    :param event_col: column holding the event information (either "0:LIVING" or "1:DECEASED")
+    :param hi_file_path: if HT index is not present in clinical data, you ccn provide the value in a separate file
+    containing the heterogeneity index list in correct order according to clinical data CSV (one value per line).
+    :param hti_threshold: Heterogeneity threshold value(s). Provide one value for binary split or two values for
+    ternary split. You can also perform binary split, when giving two values. In that case, the groups are build from
+    upper and lower bonds.
+    :param split_mode: Choose 'binary' for two groups or 'ternary' for three groups based on HI thresholds.
+    """
     # Load HI list if provided
     hi_list = None
     if hi_file_path and os.path.exists(hi_file_path):
@@ -94,22 +133,18 @@ def main(file_path, hi_file_path=None):
             hi_list = [float(line.strip()) for line in file.readlines()]
 
     # Prepare the data
-    km_data = prepare_data(file_path, hi_list, hti_threshold=hti_threshold)
-
-    # Plot Kaplan-Meier curves
-    # plot = plot_kaplan_meier(km_data, 'Overall Survival (Months)', 'Overall Survival Status',
-    #                          'HI Group')
+    km_data = prepare_data(file_path, time_col, event_col, hi_list, hti_threshold=hti_threshold, split_mode=split_mode)
 
     # Perform log-rank test
-    alpha = 0.05
     log_rank_results = perform_logrank_test(km_data, 'Overall Survival (Months)',
-                                            'Overall Survival Status', 'HI Group',
-                                            hti_threshold=hti_threshold)
+                                            'Overall Survival Status', 'HI Group')
+
     print(log_rank_results)
+    alpha = 0.05
     if log_rank_results.p_value <= alpha:
-        print(f"p value {log_rank_results.p_value} significant with significance level {alpha}")
+        print(f"p-value {log_rank_results.p_value} significant with significance level {alpha}")
     else:
-        print(f"p value {log_rank_results.p_value} \033[1mnot\033[0m significant with significance level {alpha}.")
+        print(f"p-value {log_rank_results.p_value} \033[1mnot\033[0m significant with significance level {alpha}.")
 
     # Plot Kaplan-Meier curves
     group_sizes = km_data['HI Group'].value_counts().sort_index().tolist()
@@ -118,22 +153,32 @@ def main(file_path, hi_file_path=None):
 
 
 if __name__ == "__main__":
-    # add filepaths as command line arguments or manipulate default strings further down
-    # filepath 1: clinical data file holding survival information and optionally a column holding the heterogeneity
-    # value
-    # with a column name from 'potential_colnames_HTI'
-    # filepath 2: if HTI is not present, you cn provide the value in a separate file containing the heterogeneity
-    # index list in correct order according to clinical data CSV (one value per line).
-
     parser = argparse.ArgumentParser(description="Kaplan-Meier Survival Analysis")
-    parser.add_argument("--file_path", type=str,
+    parser.add_argument("--clinical_csv", type=str,
                         default='../../..//res/merged_clinical_mirna_data.csv',
                         help="Path to the CSV file containing the data.")
+    parser.add_argument("--time_col", type=str,
+                        default='Overall Survival (Months)',
+                        help="Time column name in csv stating survival duration e. g. in months.")
+    parser.add_argument("--event_col", type=str,
+                        default='Overall Survival Status',
+                        help="Event column name in csv stating status either being:"
+                             "\n0:LIVING"
+                             "\nor"
+                             "\n1:DECEASED")
     parser.add_argument("--hi_file_path", type=str,
                         default=None,
-                        help="Path to a file containing the heterogeneity index list in correct order according to "
-                             "clinical data CSV (one value per line).")
+                        help="Path to a file containing the heterogeneity index list (one value per line).")
+    parser.add_argument("--hti_threshold", type=float, nargs='+',
+                        # default=[0.5],
+                        default=[0.3, 0.7],
+                        help="Heterogeneity threshold value(s). Provide one value for binary split or two values for ternary split.")
+    parser.add_argument("--split_mode", type=str, choices=['binary', 'ternary'],
+                        # default='binary',
+                        default='ternary',
+                        help="Choose 'binary' for two groups or 'ternary' for three groups based on HI thresholds.")
 
     args = parser.parse_args()
 
-    main(args.file_path, args.hi_file_path)
+    main(args.clinical_csv, args.time_col, args.event_col, args.hi_file_path, hti_threshold=args.hti_threshold,
+         split_mode=args.split_mode)
